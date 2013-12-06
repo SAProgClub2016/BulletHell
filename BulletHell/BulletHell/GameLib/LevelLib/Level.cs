b6798a908888b2260792b0908117b79d63ee5fde
jragonfyre <jargon.schuchardt@gmail.com>
2013-12-06 01:57:46 -0500

    Minor change, working on level

--- BulletHell/BulletHell/GameLib/LevelLib/Level.cs
+++ BulletHell/BulletHell/GameLib/LevelLib/Level.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BulletHell.GameLib.EntityLib;
+
 
 namespace BulletHell.GameLib.LevelLib
 {
     public class Level
     {
-
+        EntitySpawner spawner;
     }
 }
